2ad6669a706be2cf78b78806f8b94a4054ceb242
Semyart <olsemykin@gmail.com>
2021-08-19 10:41:15 +0300

    reworked header + hero

--- js/animations.js
+++ js/animations.js
@@ -4,11 +4,65 @@ document.addEventListener('DOMContentLoaded', function() {
     loop: true,
   });
 
-  const element = document.querySelectorAll('.top-bar__select');
-  element.forEach(element => {
-    const choices = new Choices(element, {
-      searchEnabled: false,
-      itemSelectText: '',
-    });
+  document.querySelector('#first-select-button').addEventListener('click', function() {
+    document.querySelector('#first-dropdown').classList.toggle('search-item__dropdown_is-active')
+    document.querySelector('#first-select-button').classList.toggle('search-item__button_is-active')
+    document.querySelector('#first-arrow-bottom').classList.toggle('arrow-bottom_rotate')
+    document.querySelector('#first-arrow-bottom-path').classList.toggle('arrow-bottom-path_active')
   });
+
+  document.querySelector('#second-select-button').addEventListener('click', function() {
+    document.querySelector('#second-dropdown').classList.toggle('search-item__dropdown_is-active')
+    document.querySelector('#second-select-button').classList.toggle('search-item__button_is-active')
+    document.querySelector('#second-arrow-bottom').classList.toggle('arrow-bottom_rotate')
+    document.querySelector('#second-arrow-bottom-path').classList.toggle('arrow-bottom-path_active')
+  });
+
+  document.querySelector('#third-select-button').addEventListener('click', function() {
+    document.querySelector('#third-dropdown').classList.toggle('search-item__dropdown_is-active')
+    document.querySelector('#third-select-button').classList.toggle('search-item__button_is-active')
+    document.querySelector('#third-arrow-bottom').classList.toggle('arrow-bottom_rotate')
+    document.querySelector('#third-arrow-bottom-path').classList.toggle('arrow-bottom-path_active')
+  });
+
+  document.querySelector('#fourth-select-button').addEventListener('click', function() {
+    document.querySelector('#fourth-dropdown').classList.toggle('search-item__dropdown_is-active')
+    document.querySelector('#fourth-select-button').classList.toggle('search-item__button_is-active')
+    document.querySelector('#fourth-arrow-bottom').classList.toggle('arrow-bottom_rotate')
+    document.querySelector('#fourth-arrow-bottom-path').classList.toggle('arrow-bottom-path_active')
+  });
+
+  document.querySelector('#fifth-select-button').addEventListener('click', function() {
+    document.querySelector('#fifth-dropdown').classList.toggle('search-item__dropdown_is-active')
+    document.querySelector('#fifth-select-button').classList.toggle('search-item__button_is-active')
+    document.querySelector('#fifth-arrow-bottom').classList.toggle('arrow-bottom_rotate')
+    document.querySelector('#fifth-arrow-bottom-path').classList.toggle('arrow-bottom-path_active')
+  });
+
+
+
 })
+
+new SimpleBar(document.getElementById('first-dropdown'), {
+  scrollbarMaxSize: 28,
+});
+
+new SimpleBar(document.getElementById('second-dropdown'), {
+  scrollbarMaxSize: 28,
+});
+
+new SimpleBar(document.getElementById('third-dropdown'), {
+  scrollbarMaxSize: 28,
+});
+
+new SimpleBar(document.getElementById('fourth-dropdown'), {
+  scrollbarMaxSize: 28,
+});
+
+new SimpleBar(document.getElementById('fifth-dropdown'), {
+  scrollbarMaxSize: 28,
+});
+
+
+
+
