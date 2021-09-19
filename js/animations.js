8aba199dcbd49bf3a3b168d62aa622687ac30f7e
Semyart <olsemykin@gmail.com>
2021-09-19 13:51:43 +0300

    section gallery done

--- js/animations.js
+++ js/animations.js
@@ -1,24 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const formInput = document.querySelector('.header__search-form');
 
   //слайдер
-  const swiper = new Swiper('.swiper-container', {
+  const swiper = new Swiper('.hero__swiper-container', {
     loop: true,
   });
 
   //бургер
-  $('.header__burger').click(function(event) {
-    $('.header__burger').toggleClass('header__burger_active');
-    $('.header__nav').toggleClass('header__nav_active');
-    $('.header__link-button').toggleClass('header__link-button_active');
+  document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__burger').classList.toggle('header__burger_active');
+    document.querySelector('.header__nav').classList.toggle('header__nav_active');
+    document.querySelector('.header__link-button').classList.toggle('header__link-button_active');
   })
 
   //реализация поиска
-  document.querySelector('.search__img').addEventListener('click', function() {
-    document.querySelector('.header__search-form').classList.toggle('header__search-form_active');
+  document.querySelector('.search__img').addEventListener('click', () => {
+    formInput.classList.toggle('header__search-form_active');
     document.querySelector('.header__logo').classList.toggle('header__logo_search');
     document.querySelector('.header__burger').classList.toggle('header__burger_search');
     document.querySelector('.header-top').classList.toggle('header-top_search');
   })
+
+  //Закрытие поиска, при нажатии вне формы
+  if (document.documentElement.clientWidth > 800) {
+    const formSearchButn = document.querySelector('.header__search-button');
+    document.addEventListener('click', (e) => {
+      const target = e.target;
+      const itsSearch = target == formInput || formInput.contains(target);
+      const itsSearchButn = target == formSearchButn;
+      const searchIsActive = formInput.classList.contains('header__search-form_active');
+
+      if (!itsSearch && !itsSearchButn && searchIsActive) {
+        formInput.classList.remove('header__search-form_active');
+      }
+    })
+  }
 
   //выпадающее меню
   document.querySelectorAll('.search-item__button').forEach(item => {
@@ -29,12 +45,56 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelectorAll('.search-item').forEach(el => (el != li ) ? el.classList.remove('search-item__active') : false)
     })
 
-    item.querySelectorAll('.search-item__dropdown').forEach(element => {
-      new SimpleBar(element, {
-        scrollbarMaxSize: 28,
-      });
-    })
-
   })
 
+  document.querySelectorAll('.dropdown__list').forEach(element => {
+    new SimpleBar(element, {
+      scrollbarMaxSize: 28,
+    });
+  })
+
+  // gallery
+
+  const swiperGallery = new Swiper('.right-block__swiper-container', {
+    autoHeight: false,
+    watchOverflow: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      // when window width is >= px
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 34,
+        slidesPerGroup: 1,
+        slidesPerColumn: 1,
+      },
+      651: {
+        slidesPerView: 2,
+        spaceBetween: 34,
+        slidesPerGroup: 2,
+        slidesPerColumn: 2,
+      },
+      1201: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        slidesPerGroup: 6,
+        slidesPerColumn: 2,
+      }
+    }
+  });
+
+  const gallerySelect = document.querySelector('#gallery-select');
+  const galleryChoices = new Choices(gallerySelect, {
+    searchEnabled: false,
+    duplicateItemsAllowed: false,
+  });
 })
