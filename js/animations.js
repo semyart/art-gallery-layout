e4600e2b20865fd8b9e5f621730382e089cf58d3
Semyart <olsemykin@gmail.com>
2021-11-04 21:23:23 +0300

    diplom 1 version

--- js/animations.js
+++ js/animations.js
@@ -250,6 +250,147 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  const partnersSwiper = new Swiper('.partners__swiper-container', {
+    loop: true,
 
+    breakpoints: {
+      // when window width is >= px
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      651: {
+        slidesPerView: 2,
+        spaceBetween: 34,
+        slidesPerGroup: 2
+      },
+      801: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 50,
+      },
+      1201: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 50,
+      }
+    },
+
+    navigation: {
+      nextEl: '.partners__swiper-button-next',
+      prevEl: '.partners__swiper-button-prev',
+    },
+  });
+
+  tippy('#first-tooltip', {
+    content: 'Пример современных тенденций - современная методология разработки',
+    theme: 'purple',
+    maxWidth: 284,
+    trigger: 'click',
+  });
+
+  tippy('#second-tooltip', {
+    content: 'Приятно, граждане, наблюдать, как сделанные на базе аналитики выводы вызывают у вас эмоции',
+    theme: 'purple',
+    maxWidth: 285,
+    trigger: 'click',
+  });
+
+  tippy('#third-tooltip', {
+    content: 'В стремлении повысить качество ',
+    theme: 'purple',
+    maxWidth: 250,
+    trigger: 'click',
+  });
+
+  document.querySelectorAll('.projects__tooltip').forEach(item => item.addEventListener('click', () => {
+    item.classList.toggle('projects__tooltip_active');
+  }));
+
+  // contacts
+
+  if (window.matchMedia("(max-width: 650px)").matches) {
+    const map = document.createElement('div');
+    map.classList.add('right-block__map');
+    map.id = 'map';
+    document.querySelector('.left-block__address').after(map);
+  }
+
+  let center = [55.75955877956886,37.61707877090922];
+  let zoom = 15;
+  if (window.matchMedia("(max-width: 1200px)").matches) {
+    center = [55.75618766909688,37.71600000852678];
+    zoom = 14;
+  }
+
+  if (window.matchMedia("(max-width: 800px)").matches) {
+    center = [55.75595666520723,37.60958078613421];
+    zoom = 14;
+  }
+
+  if (window.matchMedia("(max-width: 650px)").matches) {
+    center = [55.75905317277174,37.61162089171043];
+    zoom = 14;
+  }
+
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        center: center,
+        zoom: zoom,
+        controls: [],
+    }, {
+      suppressMapOpenBlock: true,
+      geolocationControlFloat: 'right',
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.75846806898367,37.60108849999989], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/contacts/marker.svg',
+      iconImageSize: [20, 20],
+    });
+
+    if (window.matchMedia("(min-width: 1201px)").matches) {
+      myMap.controls.add('zoomControl', {
+        size: 'small',
+        position: {
+            top: '250px',
+            right: '5px'
+        }
+      });
+
+      myMap.controls.add('geolocationControl', {
+        size: 'small',
+        float: 'none',
+        position: {
+            top: '330px',
+            right: '5px'
+        }
+      });
+    }
+
+    myMap.geoObjects.add(myPlacemark);
+  };
+
+  new JustValidate('.call-form', {
+    rules: {
+      name: {
+        required: true
+      },
+      phone: {
+        required: true,
+        function: (name, value) => {
+          const phone = document.querySelector('.call-form__input_tel').value;
+          return Number(phone) && phone.length === 10
+        }
+      },
+    },
+    messages: {
+      name: 'Недопустимый формат',
+      phone: 'Недопустимый формат'
+    },
+    colorWrong: '#D11616',
+  });
 
 })
